0ef616f871a8028787dcc911fe5331da70dbf35e
Dunconius <duncanbean84@gmail.com>
2024-05-26 08:19:27 +1000

    feat: class basic functionality

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 // import library class, book class, audio-book class, games class, movies class, music class
 // if these classes have shared properties (name, year, genre, etc) make a base class and make each of these extend that class
 
+const { Book } = require("./Book");
 const { Media } = require("./Media");
+const { Library } = require("./library");
+
 
 // create instances of classes
 
-let lotr = new Media("fellowship of the ring");
+let lotr = new Book("fellowship of the ring");
 
 console.log(lotr);
+console.log(lotr.name);
+console.log(lotr.edition);
 
 // Do things with instances
 
@@ -16,4 +21,11 @@ console.log(lotr);
 // add media instances into the library instance
 
 
+let libraryInstance = new Library;
+libraryInstance.books.push(lotr);
+
 // log data in the library
+
+console.log(libraryInstance);
+
+console.log(libraryInstance.books[0].name);
