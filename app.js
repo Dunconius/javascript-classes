c51507195aef35b58ab359765543d1386a0eed2f
Dunconius <duncanbean84@gmail.com>
2024-05-26 08:08:54 +1000

    feat: class example

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// import library class, book class, audio-book class, games class, movies class, music class
+// if these classes have shared properties (name, year, genre, etc) make a base class and make each of these extend that class
+
+const { Media } = require("./Media");
+
+// create instances of classes
+
+let lotr = new Media("fellowship of the ring");
+
+console.log(lotr);
+
+// Do things with instances
+
+// create an instance of  library
+// add media instances into the library instance
+
+
+// log data in the library
